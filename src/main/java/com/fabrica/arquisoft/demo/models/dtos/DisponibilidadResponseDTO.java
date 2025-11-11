574236025d1b5fd6c67791417242e8964727aaab
Sammir Alejandro Bolanos Luna <sammir.bolanos@mvm.com.co>
2025-11-11 11:29:33 -0500

    Adicion de ids  necesarios a la ResponseDto de disponibilidad

--- src/main/java/com/fabrica/arquisoft/demo/models/dtos/DisponibilidadResponseDTO.java
+++ src/main/java/com/fabrica/arquisoft/demo/models/dtos/DisponibilidadResponseDTO.java
@@ -2,6 +2,9 @@ package com.fabrica.arquisoft.demo.models.dtos;
 
 public record DisponibilidadResponseDTO(
     Integer idDisponibilidad,
+    Integer idConsultorio,
+    Short idFranjaHoraria,
+    Integer idEspecialidad,
     Integer idProfesional,
     String nombreProfesional,
     String nombreEspecialidad,
